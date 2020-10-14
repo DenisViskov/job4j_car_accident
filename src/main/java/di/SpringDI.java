c13bfcacdd940860966a7bcbc1d0d0cbf8599c92
DenisViskov <DenisViskov16@gmail.com>
2020-10-14 21:03:10 +0300

    1. Spring DI. Запуск [#334647]

---
+++ src/main/java/di/SpringDI.java
@@ -0,0 +1,24 @@
+package di;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * Class is a spring DI
+ *
+ * @author Денис Висков
+ * @version 1.0
+ * @since 14.10.2020
+ */
+public class SpringDI {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(Store.class);
+        context.register(StartUI.class);
+        context.register(ConsoleInput.class);
+        context.refresh();
+        StartUI ui = context.getBean(StartUI.class);
+        ui.add("Petr Arsentev");
+        ui.add("Ivan ivanov");
+        ui.print();
+    }
+}
