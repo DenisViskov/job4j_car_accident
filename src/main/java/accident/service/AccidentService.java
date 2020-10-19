30f66c99184fae4c6073e5b90423985d9330a856
DenisViskov <denisviskov16@gmail.com>
2020-10-19 16:22:12 +0300

    2. IndexControl. Таблица и вид. [#334641]

---
+++ src/main/java/accident/service/AccidentService.java
@@ -0,0 +1,12 @@
+package accident.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Денис Висков
+ * @version 1.0
+ * @since 19.10.2020
+ */
+@Service
+public class AccidentService {
+}
