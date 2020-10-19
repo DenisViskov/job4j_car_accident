da79fad5bb05f10b86e30b5fce23091aee6c9bad
DenisViskov <denisviskov16@gmail.com>
2020-10-19 15:49:43 +0300

    1. Загрузка данных в вид [#334636]

--- src/main/java/accident/control/IndexControl.java
+++ src/main/java/accident/control/IndexControl.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 /**
  * @author Денис Висков
  * @version 1.0
@@ -13,6 +15,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class IndexControl {
     @GetMapping("/")
     public String index(Model model) {
+        List<String> list = List.of("Petr Arsentev", "Denis Viskov");
+        model.addAttribute("user", list);
         return "index";
     }
 }
