51358e609873f1fb93a635fb6e2ef43ddbf613da
DenisViskov <denisviskov16@gmail.com>
2020-10-19 14:32:39 +0300

    4. Время жизни (Scope) [#334645]

--- src/main/java/di/StartUI.java
+++ src/main/java/di/StartUI.java
@@ -1,6 +1,7 @@
 package di;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -13,6 +14,7 @@ import java.io.IOException;
  * @since 14.10.2020
  */
 @Component
+@Scope("prototype")
 public class StartUI {
     /**
      * Store
