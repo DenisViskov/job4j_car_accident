f3ff578e3b2659089e703d50f0694c6146b17091
DenisViskov <denisviskov16@gmail.com>
2020-10-23 16:27:07 +0300

    2. Spring Data [#334658]

--- src/main/java/accident/control/IndexControl.java
+++ src/main/java/accident/control/IndexControl.java
@@ -1,12 +1,18 @@
 package accident.control;
 
+import accident.model.Accident;
+import accident.model.AccidentEntity;
 import accident.repository.AccidentHibernate;
 import accident.repository.AccidentJdbcTemplate;
+import accident.repository.AccidentRepository;
 import accident.service.RepositoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Денис Висков
@@ -16,15 +22,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexControl {
 
-    private final AccidentHibernate accidents;
+    private final AccidentRepository accidents;
 
-    public IndexControl(AccidentHibernate accidents) {
+    public IndexControl(AccidentRepository accidents) {
         this.accidents = accidents;
     }
 
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("accidents", accidents.findAll());
+        List<AccidentEntity> res = new ArrayList<>();
+        accidents.findAll().forEach(res::add);
+        model.addAttribute("accidents", res);
         return "index";
     }
 }
