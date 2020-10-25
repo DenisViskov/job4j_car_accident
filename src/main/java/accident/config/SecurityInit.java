5f1253e5233b46b911781450e80a9e8a35f59f83
DenisViskov <denisviskov16@gmail.com>
2020-10-25 16:52:10 +0300

    0. Spring Security [#334652]

---
+++ src/main/java/accident/config/SecurityInit.java
@@ -0,0 +1,11 @@
+package accident.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * @author Денис Висков
+ * @version 1.0
+ * @since 25.10.2020
+ */
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
